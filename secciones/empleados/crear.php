7acf37f7adf50fe77abbb2b026a9dde64329de9d
Dare044 <95920031+Dare044@users.noreply.github.com>
2023-05-05 19:30:48 +0200

    Update crear.php

--- secciones/empleados/crear.php
+++ secciones/empleados/crear.php
@@ -1,3 +1,69 @@
+<?php
+
+include("../../bd.php");
+
+if($_POST){
+
+    $primernombre=(isset($_POST["primernombre"])?$_POST["primernombre"]:"");
+    $segundonombre=(isset($_POST["segundonombre"])?$_POST["segundonombre"]:"");
+    $primerapellido=(isset($_POST["primerapellido"])?$_POST["primerapellido"]:"");
+    $segundoapellido=(isset($_POST["segundoapellido"])?$_POST["segundoapellido"]:"");
+
+    $foto=(isset($_FILES["foto"]["name"])?$_FILES["foto"]["name"]:"");
+    $cv=(isset($_FILES["cv"]["name"])?$_FILES["cv"]["name"]:"");
+
+    $idpuesto=(isset($_POST["idpuesto"])?$_POST["idpuesto"]:"");
+    $fechadeingreso=(isset($_POST["fechadeingreso"])?$_POST["fechadeingreso"]:"");
+
+
+
+    $sentencia = $conexion->prepare("INSERT INTO
+    `tbl_empleados` (`id`, `primernombre`, `segundonombre`, `primerapellido`, `segundoapellido`, `foto`, `cv`, `idpuesto`, `fechadeingreso`) 
+    VALUES (NULL, :primernombre,:segundonombre,:primerapellido,:segundoapellido,:foto,:cv,:idpuesto,:fechadeingreso);");
+
+    $sentencia->bindParam(":primernombre", $primernombre);
+    $sentencia->bindParam(":segundonombre", $segundonombre);
+    $sentencia->bindParam(":primerapellido", $primerapellido);
+    $sentencia->bindParam(":segundoapellido", $segundoapellido);
+
+    $fecha_=new DateTime();
+    $nombreArchivo_foto=($foto!='')?$fecha_->getTimestamp()."_".$_FILES["foto"]["name"]:"";
+    $tmp_foto=$_FILES["foto"]["tmp_name"];
+    if($tmp_foto!=''){
+      move_uploaded_file($tmp_foto,"./".$nombreArchivo_foto);
+    }
+    $sentencia->bindParam(":foto", $nombreArchivo_foto);
+
+
+    $nombreArchivo_cv=($cv!='')?$fecha_->getTimestamp()."_".$_FILES["cv"]["name"]:"";
+    $tmp_cv=$_FILES["cv"]["tmp_name"];
+    if($tmp_cv!=''){
+      move_uploaded_file($tmp_cv,"./".$nombreArchivo_cv);
+    }
+    $sentencia->bindParam(":cv", $nombreArchivo_cv);
+
+
+    $sentencia->bindParam(":idpuesto", $idpuesto);
+    $sentencia->bindParam(":fechadeingreso", $fechadeingreso);
+    
+    
+    $sentencia->execute();
+    
+    header("Location:index.php");
+}
+
+$sentencia=$conexion->prepare("SELECT * FROM tbl_puestos");
+$sentencia->execute();
+$lista_tbl_puestos=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+?>
+
+
+
+
+
 <?php include("../../templates/header.php"); ?>
   <br/>
 <div class="card">
@@ -34,23 +100,25 @@
 
             <div class="mb-3">
               <label for="" class="form-label">Foto:</label>
-              <input type="text"
+
+              <input type="file"
                 class="form-control" name="foto" id="foto" aria-describedby="helpId" placeholder="Foto">
             </div>
 
             <div class="mb-3">
               <label for="cv" class="form-label">CV(PDF):</label>
-              <input type="text"
+              <input type="file"
                 class="form-control" name="cv" id="cv" aria-describedby="helpId" placeholder="CV">
             </div>
 
             <div class="mb-3">
                 <label for="idpuesto" class="form-label">Puesto:</label>
                 <select class="form-select form-select-lg" name="idpuesto" id="idpuesto">
-                    <option selected>Select one</option>
-                    <option value="">New Delhi</option>
-                    <option value="">Istanbul</option>
-                    <option value="">Jakarta</option>
+                <?php foreach ($lista_tbl_puestos as $registro) { ?>
+    
+                <option value="<?php echo $registro['id']?>"><?php echo $registro['nombredelpuesto']?></option>
+
+                <?php  } ?>
                 </select>
             </div>
 
@@ -61,7 +129,7 @@
 
 
 
-            <button type="button" class="btn btn-success">Agregar Registro</button>
+            <button type="submit" class="btn btn-success">Agregar Registro</button>
             <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
         </form>
 
