b94a417186cad0bc9fd1ad5029c8d1bce22619e4
Djordje261 <95472719+Djordje261@users.noreply.github.com>
2023-05-05 21:53:03 +0200

    aaa

---
+++ secciones/empleados/carta.php
@@ -0,0 +1,65 @@
+<?php
+
+include("../../bd.php");
+
+if(isset( $_GET['txtID'])) {
+    $txtID=(isset($_GET['txtID']))?$_GET['txtID']:"";
+
+
+    $sentencia=$conexion->prepare("SELECT * FROM tbl_empleados WHERE id=:id");
+    $sentencia->bindParam(":id", $txtID);
+    $sentencia->execute();
+    $registro=$sentencia->fetch(PDO::FETCH_LAZY);
+
+
+
+     print_r($registro);
+
+    $primernombre=$registro["primernombre"];
+    $segundonombre=$registro["segundonombre"];
+    $primerapellido=$registro["primerapellido"];
+    $segundoapellido=$registro["segundoapellido"];
+   
+   $nombreCompleto=$primernombre." ".$segundonombre." ".$primerapellido." ". $segundoapellido;
+   
+    $foto=$registro["foto"];
+    $cv=$registro["cv"];
+    $idpuesto=$registro["idpuesto"];
+    $fechadeingreso=$registro["fechadeingreso"]; 
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content=""width="device-width, initial-scale=1.0">
+
+
+
+	<title>Carta de Recomendación</title>
+</head>
+<body>
+	<h1>   Carta de Recomendación </h1>
+</br></br>
+<p>Estimado Jose Ángel,</p>
+
+<p>Me complace escribir esta carta de recomendación para <strong><? php echo $nombreCompleto;?></strong>. Conozco a <? php echo $nombreCompleto;?> desde hace muchos años, y he tenido el placer de trabajar con él en el proyecto XIIA.</p>
+
+<p>Como Jefe de Departamento de XIIA, he tenido la oportunidad de observar las habilidades y el desempeño de <? php echo $nombreCompleto;?> de cerca. En mi opinión, él es un profesional muy competente y responsable, con una gran habilidad para ser proactivo y analítico. Él es capaz de resolver problemas, tomar decisiones y liderar proyectos con gran eficacia y eficiencia.</p>
+
+<p>Además, <? php echo $nombreCompleto;?> es una persona muy creativa, innovadora y comunicativa, lo que le permite trabajar bien en equipo y tener una excelente relación con sus colegas. Él siempre ha sido muy enérgico y trabajador en su labor, y ha demostrado ser un colaborador muy confiable y orientado a resultados, con un fuerte compromiso con la calidad y la excelencia.</p>
+
+<p>En resumen, recomiendo a <? php echo $nombreCompleto;?> sin reservas para cualquier posición que esté buscando. Él sería un activo valioso para cualquier empresa y estoy seguro/a de que hará una contribución significativa en cualquier trabajo que tenga.</p>
+
+<p>No dude en ponerse en contacto conmigo si necesita más información sobre David o su trabajo en XIIA.</p>
+
+<p>Sinceramente,</p>
+
+	<p><strong>Daniel Requena</strong></p>
+	<p><strong>Programador Jr.</strong></p>
+	<p><strong>XIIA</strong></p>
+</body>
+</html>
---
+++ secciones/puestos/crear.php
@@ -0,0 +1,58 @@
+<?php
+
+include("../../bd.php");
+
+if($_POST){
+    print_r($_POST);
+
+    //Recolectamos los datos del POST
+    $nombredelpuesto=(isset($_POST["nombredelpuesto"])?$_POST["nombredelpuesto"]:"");
+    //Preparar la insercion de los dartos
+    $sentencia = $conexion->prepare("INSERT INTO tbl_puestos(id,nombredelpuesto) 
+            VALUES (null, :nombredelpuesto)");
+
+
+    $sentencia->bindParam(":nombredelpuesto", $nombredelpuesto);
+    $sentencia->execute();
+    header("Location:index.php");
+}
+
+?>
+
+<?php include("../../templates/header.php"); ?>
+<br/>
+
+<div class="card">
+    <div class="card-header">
+        Puestos
+    </div>
+    <div class="card-body">
+         
+    <form action="" method="post" enctype="multipart/form-data">
+
+
+    <div class="mb-3">
+      <label for="nombredelpuesto" class="form-label">Nombre del puesto </label>
+      <input type="text"
+        class="form-control" name="nombredelpuesto" id="nombredelpuesto" aria-describedby="helpId" placeholder="Nombre del puesto">
+    </div>
+
+    <button type="submit" class="btn btn-sucess">Agregar</button>
+    <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+
+    </form>
+    
+
+
+    </div>
+    <div class="card-footer text-muted">
+
+    </div>
+</div>
+  
+
+
+
+
+
+<?php include("../../templates/footer.php"); ?>
